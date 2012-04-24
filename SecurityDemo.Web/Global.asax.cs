fbb8b119673a8541ef7c2acdc13910c7f8091226
Sam Critchley <critchley.sj@gmail.com>
2012-04-24 22:27:51 +1000

    changed default route

--- SecurityDemo.Web/Global.asax.cs
+++ SecurityDemo.Web/Global.asax.cs
@@ -24,7 +24,7 @@ namespace SecurityDemo.Web
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Demo", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
